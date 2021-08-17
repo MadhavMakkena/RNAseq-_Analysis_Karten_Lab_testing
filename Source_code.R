041e748352dcd7c342a5dd8940b11b02edd3689b
Madhav Makkena <madhav.makkena@gmail.com>
2021-08-16 22:53:05 -0300

    cleanup

--- Source_code.R
+++ Source_code.R
@@ -23,18 +23,16 @@ all (rownames(sample_data) == colnames(count_data))
 
 
 #cleaning up count_data to remove genes with 0 counts in all samples
-count_data_clean <- count_data
-count_data_clean <- count_data_clean[rowSums(count_data_clean) !=0, ]
+count_data<- count_data[rowSums(count_data) !=0, ]
 #viewing the first 10 records to check
 head(count_data,10)
-head(count_data_clean,10)
 
 
 
 #DESeq analysis
 library(DESeq2)
 #storing the input values from count_data_clean
-DESeq_input <- DESeqDataSetFromMatrix(countData = count_data_clean, colData = sample_data, design = ~ condition)
+DESeq_input <- DESeqDataSetFromMatrix(countData = count_data, colData = sample_data, design = ~ condition)
 
 
 
@@ -62,34 +60,37 @@ hist(exp_60_sort, breaks = 1000000, col = "grey", xlim=c(0,100), ylim=c(0,100))
 
 
 #performing DESeq2 on exp_100_sort
-DESeq_run_exp_100 <-DESeq(exp_100)
-plotDispEsts(DESeq_run_exp_100)
+DESeq_100 <-DESeq(exp_100)
+plotDispEsts(DESeq_100)
 #
 #performing DESeq2 on exp_60_sort
-DESeq_run_exp_60 <-DESeq(exp_60)
-plotDispEsts(DESeq_run_exp_60)
-
+DESeq_60 <-DESeq(exp_60)
+plotDispEsts(DESeq_60)
+#getting the conditions of DESeq
+cond<-as.character(DESeq_100$condition)
 
 
 #normalising the counts exp_100
-normal_count_exp_100 <-counts(DESeq_run_exp_100, normalized=TRUE)
+normal_count_exp_100 <-counts(DESeq_100, normalized=TRUE)
+counts_100 <- as.data.frame(normal_count_exp_100)
 normal_count_exp_100
+counts_100
 #
 #normalising the counts exp_60
-normal_count_exp_60 <-counts(DESeq_run_exp_60, normalized=TRUE)
+normal_count_exp_60 <-counts(DESeq_60, normalized=TRUE)
+counts_60 <- as.data.frame(normal_count_exp_60)
 normal_count_exp_60
+counts_60
 
 
 
-#extracting the results from DESeq_run_exp_100
-result_exp100 <- results(DESeq_run_exp_100)
-result_exp100
-write.csv( as.data.frame(result_exp100), file="result_exp100.csv")
+#extracting the results from DESeq_100
+result_100 <- results(DESeq_100)
+result_100
 #
-#extracting the results from DESeq_run_exp_60
-result_exp60 <- results(DESeq_run_exp_60)
-result_exp60
-write.csv( as.data.frame(result_exp60), file="result_exp60.csv")
+#extracting the results from DESeq_60
+result_60 <- results(DESeq_60)
+result_60
 
 
 
@@ -105,44 +106,93 @@ library(BinfTools)
 
 
 
-# dds is DESeq_run_exp_100 or DESeq_run_exp_60
-# res is result_exp100 or result_exp60
+# dds is DESeq_100 or DESeq_60
+# res is result_100 or result_60
+# counts is counts_100 or counts_60
 #
 # getting HGNC Names from ENSG names for result_exp100
-sym_result_exp100 <- result_exp100
-sym_result_exp100 <- getSym(object=sym_result_exp100,
+sym_result_100 <- result_100
+sym_result_100 <- getSym(object=sym_result_100,
                obType="res",
                species="hsapiens",
                target="HGNC",
                addCol=F)
-head(sym_result_exp100)
+head(sym_result_100)
 #
 # getting HGNC Names from ENSG names for result_exp60
-sym_result_exp60 <- result_exp60
-sym_result_exp60 <- getSym(object=sym_result_exp60,
-                            obType="res",
-                            species="hsapiens",
-                            target="HGNC",
-                            addCol=F)
-head(sym_result_exp60)
+sym_result_60 <- result_60
+sym_result_60 <- getSym(object=sym_result_60,
+                         obType="res",
+                         species="hsapiens",
+                         target="HGNC",
+                         addCol=F)
+head(sym_result_60)
+#
+# getting HGNC Names from ENSG names for counts_100
+sym_counts_100 <- counts_100
+sym_counts_100 <- getSym(object=sym_counts_100,
+                        obType="counts",
+                        species="hsapiens",
+                        target="HGNC",
+                        addCol=F)
+head(sym_counts_100)
+#
+# getting HGNC Names from ENSG names for counts_60
+sym_counts_60 <- counts_60
+sym_counts_60 <- getSym(object=sym_counts_60,
+                        obType="counts",
+                        species="hsapiens",
+                        target="HGNC",
+                        addCol=F)
+head(sym_counts_60)
 
 
 
-#specific gene sets onloading
+#specific gene sets onloading $V1 takes the first row and reads it as a vector
 Gene_set_chol_biosyn <- read.csv('Gene_set_cholesterol_biosynthesis.csv', header = FALSE)$V1
 Gene_set_chol_homeo <- read.csv('Gene_set_cholesterol_homeostasis.csv', header = FALSE)$V1
 Gene_set_KEGG_endocytosis <- read.csv('Gene_set_KEGG_endocytosis.csv', header = FALSE)$V1
 #
-#gene sets exp100
-exp100_Gene_set_chol_biosyn <- subset(sym_result_exp100, rownames(sym_result_exp100) %in% Gene_set_chol_biosyn)
-exp100_Gene_set_chol_homeo <- subset(sym_result_exp100, rownames(sym_result_exp100) %in% Gene_set_chol_homeo)
-exp100_Gene_set_KEGG_endocytosis <- subset(sym_result_exp100, rownames(sym_result_exp100) %in% Gene_set_KEGG_endocytosis)
-#
-#gene sets exp100
-exp60_Gene_set_chol_biosyn <- subset(sym_result_exp60, rownames(sym_result_exp60) %in% Gene_set_chol_biosyn)
-exp60_Gene_set_chol_homeo <- subset(sym_result_exp60, rownames(sym_result_exp60) %in% Gene_set_chol_homeo)
-exp60_Gene_set_KEGG_endocytosis <- subset(sym_result_exp60, rownames(sym_result_exp60) %in% Gene_set_KEGG_endocytosis)
+# #gene sets exp100
+# exp100_Gene_set_chol_biosyn <- subset(sym_result_exp100, rownames(sym_result_exp100) %in% Gene_set_chol_biosyn)
+# exp100_Gene_set_chol_homeo <- subset(sym_result_exp100, rownames(sym_result_exp100) %in% Gene_set_chol_homeo)
+# exp100_Gene_set_KEGG_endocytosis <- subset(sym_result_exp100, rownames(sym_result_exp100) %in% Gene_set_KEGG_endocytosis)
+# #
+# #gene sets exp100
+# exp60_Gene_set_chol_biosyn <- subset(sym_result_exp60, rownames(sym_result_exp60) %in% Gene_set_chol_biosyn)
+# exp60_Gene_set_chol_homeo <- subset(sym_result_exp60, rownames(sym_result_exp60) %in% Gene_set_chol_homeo)
+# exp60_Gene_set_KEGG_endocytosis <- subset(sym_result_exp60, rownames(sym_result_exp60) %in% Gene_set_KEGG_endocytosis)
+
+
+
+# volcano plots and MA plots use either sym_result_100 or sym_result_60
+# barGene(), count_plot(), and gsva_plot() use either sym_counts_100 or sym_counts_100
 
 
 
 
+
+
+
+
+dim(sym_result_exp100)
+dim(cond)
+view(cond)
+cond<-as.character(DESeq_run_exp_100$condition)
+barGene(genes=Gene_set_chol_biosyn,
+        counts=sym_result_exp100,
+        conditions=cond,
+        title="sym_result_exp100",
+        norm="WT",
+        eb="se",
+        col=c("blue","yellow"),
+        ord=NULL)
+
+barGene(genes=Gene_set_chol_biosyn,
+        counts=log10(1+counts_exp100),
+        conditions=cond,
+        title="sym_result_exp100",
+        norm="WT",
+        eb="se",
+        col=c("blue","yellow"),
+        ord=NULL)
